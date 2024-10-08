c27cec54b3eebad617b0ca422cc11c21545fa5f6
Ashik <138590481+ashik-ece@users.noreply.github.com>
2024-10-08 18:28:10 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,37 @@
+"use strict"; // treat all JS code as newer version
+//alert("hello") // we are using nodejs not browser
+
+
+// sidenote : Try to ensure code readability is high. All of the below codes will run, but redability is an issue
+
+console.log(3
+    +3) // poor redability
+
+console.log("AShik"); console.log("123") // poor redability
+
+// good redability
+
+console.log("Ashik")
+console.log("123")
+
+
+//datatype
+
+let name = "ASHIK" // string
+let age = 18 //  number
+let isLoggedIn = false // boolean
+let state; // undefined
+
+//number 
+//bigInt - for numbers which are very very huge
+//string => "" or ''. // " " is prefered
+// bool => true/false
+//null => standalone value type
+//undefined => values are yet to be assigned
+//symbol => unique
+
+//object 
+
+console.log(typeof age); // type - number
+console.log(typeof null); // type - object
+console.log(typeof undefined); // type - undefined
